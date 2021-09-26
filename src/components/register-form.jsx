ea20629ea8555cb08e359e81da9df3e6ae1ef3a3
Darasimi Ajewole <darasimi@formpl.us>
2021-09-26 19:18:05 +0100

    Added flow for recording offline response Added flow for synching offline response Added flow for persisting data between page reloads

--- src/components/register-form.jsx
+++ src/components/register-form.jsx
@@ -1,25 +1,75 @@
 import Input from './input';
-import Radio from './radio';
+import RadioGroup from './radio-group';
+import { useEffect, useCallback, } from 'react';
+import { CREATE_RESPONSE, UPDATE_RESPONSE, RECORD_RESPONSE, submitResponseAction } from '../actions';
+import { useDispatch, useSelector } from 'react-redux'
+import { getResponse } from '../model';
+import { v4 as uuidv4 } from 'uuid';
 
-const Form = () => (
-  <form className="row register-form">
-    <div className="col-md-6">
-      <Input placeholder="First Name *" type="text" />
-      <Input placeholder="Last Name *" type="text" />
-      <div className="form-group">
-        <div className="maxl">
-          <Radio label="Male" name="gender" value="male" />
-          <Radio label="Female" name="gender" value="female" />
-          <Radio label="Prefer not to say" name="gender" value="Prefer not to say" checked={true} />
-        </div>
+const Form = ({ responseId }) => {
+  const dispatch = useDispatch()
+  const response = useSelector((state) => getResponse(state, responseId))
+  const online = useSelector((state) => state.offline.online)
+
+  useEffect(() => {
+    if (response) return
+    dispatch({ type: CREATE_RESPONSE, payload: { id: responseId } })
+  }, [response, responseId, dispatch])
+
+  const handleChange = useCallback(
+    (name, value) => dispatch({
+      type: UPDATE_RESPONSE,
+      payload: { [name]: value, id: responseId }
+    }),
+    [dispatch, responseId],
+  );
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const newResponse = responseId === newResponseId
+
+    if (newResponse) {  // record response first
+      const recordResponseId = uuidv4()
+      dispatch({ type: RECORD_RESPONSE, payload: { id: responseId, recordResponseId } })
+      response.id = recordResponseId;
+    }
+    dispatch(submitResponseAction(response, online))
+  }
+
+  if (!response) {
+    return null // this can be some loader or some messaging
+  }
+
+
+  return (
+    <form className="row register-form" onSubmit={handleSubmit}>
+      <div className="col-md-6">
+        <Input value={response.firstName} type="text" name="firstName" onChange={handleChange} placeholder="First Name *" />
+        <Input value={response.lastName} type="text" name="lastName" onChange={handleChange} placeholder="Last Name *" />
+        <RadioGroup options={GENDERS} selected={response.gender} name="gender" onChange={handleChange} />
       </div>
-    </div>
-    <div className="col-md-6">
-      <Input placeholder="Your Email *" type="email" />
-      <Input placeholder="Your Phone *" type="text" />
-      <input type="submit" className="btnRegister" defaultValue="Register" />
-    </div>
-  </form>
-)
+
+
+      <div className="col-md-6">
+        <Input value={response.email} placeholder="Your Email *" type="email" name="email" onChange={handleChange} />
+        <Input value={response.phone} placeholder="Your Phone *" type="text" name="phone" onChange={handleChange} />
+        <input type="submit" className="btnRegister" defaultValue="Register" />
+      </div>
+
+    </form>
+  )
+}
+
+const newResponseId = 'latest'
+
+Form.defaultProps = {
+  responseId: newResponseId,
+}
+
+const GENDERS = [
+  { label: 'Male', value: 'male' },
+  { label: 'Female', value: 'female' },
+  { label: 'Prefer not to say', value: 'Prefer not to say' }
+]
 
 export default Form
