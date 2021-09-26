db2b4632922c8a05606c1f56b6eccecc7503bb5a
Darasimi Ajewole <darasimi@formpl.us>
2021-09-26 19:13:06 +0100

    Added flow for recording offline response Added flow for synching offline response Added notficiation bar

--- src/model.js
+++ src/model.js
@@ -1,29 +1,58 @@
 import { Model, ORM } from "redux-orm";
-import { CREATE_FORM, UPDATE_FORM } from "./actions";
+import {
+  CREATE_RESPONSE, UPDATE_RESPONSE,
+  RESPONSE_SUBMITTED, RECORD_RESPONSE,
+} from "./actions";
 import { createSelector } from 'redux-orm';
+import { JS_ERROR } from '@redux-offline/redux-offline/lib/constants';
+import { toast } from 'react-toastify';
 
-class Form extends Model {
-  static reducer(action, Form) {
-    let form;
+export class Response extends Model {
+  static reducer(action, Response) {
+    let response;
     switch (action.type) {
-      case CREATE_FORM:
-        Form.create(action.payload);
+      case CREATE_RESPONSE:
+        Response.create(action.payload);
         break;
-      case UPDATE_FORM:
-        form = Form.withId(action.payload.id);
-        form.update(action.payload);
+
+      case UPDATE_RESPONSE:
+        response = Response.withId(action.payload.id);
+        response.update(action.payload);
         break;
+
+      case RECORD_RESPONSE:
+        const { id, recordResponseId } = action.payload;
+        // Clone response and store
+        const oldResponse = Response.withId(id);
+        const recordedResponse = { ...oldResponse.ref, id: recordResponseId, recorded: new Date() }
+        Response.create(recordedResponse)
+        oldResponse.delete()
+        break;
+
+      case RESPONSE_SUBMITTED:
+        response = Response.withId(action.meta.id);
+        toast.info(`Response saved at ${response.recorded} submitted successfully`)
+        response.update({ submitted: new Date() });
+        break
+
+      case JS_ERROR:
+        console.error(action?.meta?.error)
+        break
       default:
         break
     }
   }
 }
-Form.modelName = 'Form';
+
+Response.modelName = 'Response';
 
 const orm = new ORM({
   stateSelector: state => state.orm,
 });
-orm.register(Form);
+orm.register(Response);
 
 export default orm
-export const getForm = createSelector(orm.Form);
+export const getResponse = createSelector(orm.Response);
+export const getPendingResponseCount = createSelector([orm], (session) =>
+  session.Response.all().filter(response => response.recorded && !response.submitted).count()
+)
