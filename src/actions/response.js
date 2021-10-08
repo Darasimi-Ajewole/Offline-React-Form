e64b32c73cd30941dbf32600af8327785f12b631
Darasimi Ajewole <darasimi@formpl.us>
2021-10-08 08:33:08 +0100

    Implemented upload of pending files along with submission

--- src/actions/response.js
+++ src/actions/response.js
@@ -1,5 +1,8 @@
 import submitRequest from "../utils/submit";
+import { uploadFileAction } from "./fileupload";
 import { toast } from 'react-toastify';
+import localforage from 'localforage';
+import { getFile } from "../model/fileupload";
 export const UPLOAD_FILE = 'UPLOAD_FILE';
 export const CREATE_RESPONSE = 'CREATE_RESPONSE';
 export const UPDATE_RESPONSE = 'UPDATE_RESPONSE';
@@ -9,20 +12,33 @@ export const RESPONSE_SUBMITTED = 'RESPONSE_SUBMITTED';
 export const FAILED_SUBMISSION = 'FAILED_SUBMISSION';
 export const DELETE_RESPONSE = 'DELETE_RESPONSE';
 
-export const submitResponseAction = (response, online = true) => {
-  const toastMsg = online ? 'Submitting Response' : 'Response Recorded'
+export const submitResponseAction = (response, online = true) => async (dispatch, getState) => {
+  const customRequest = async ({ url, json }) => {
+    const displayPic = getFile(getState(), response.displayPic)
+    if (displayPic) {
+      const uploaded = Boolean(displayPic.uploadData)
+      if (!uploaded) {
+        const displayPicBlob = await localforage.getItem(displayPic.id)
+        await dispatch(uploadFileAction(displayPic.id, displayPicBlob))
+      }
+      localforage.removeItem(displayPic.id)
+    }
+    return await submitRequest({ url, json })
+  }
+
+  const toastMsg = online ? 'Submitting Response' : 'Response Recorded' // modify the loader of this to match upload progress
   toast.info(toastMsg)
-  return {
+  dispatch({
     type: SUBMIT_RESPONSE,
     payload: response,
     meta: {
       offline: {
-        effect: { url: "https://jsonplaceholder.typicode.com/posts", json: response, customRequest: submitRequest },
+        effect: { url: "https://jsonplaceholder.typicode.com/posts", json: response, customRequest },
         // action to dispatch when effect succeeds:
         commit: { type: RESPONSE_SUBMITTED, meta: { id: response.id } },
         // action to dispatch if network action fails permanently:
         rollback: { type: FAILED_SUBMISSION, meta: { id: response.id } }
       }
     }
-  }
+  })
 }
